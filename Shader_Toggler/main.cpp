f41051e1c265f1d3ac89152ad1f4dcd08d6ae90d
skewerb <skewerblack@gmail.com>
2014-09-21 14:31:32 -0700

    Hello World
    Doing the most basic operation with QT

--- Shader_Toggler/main.cpp
+++ Shader_Toggler/main.cpp
@@ -1,11 +1,16 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QPushButton>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+    //MainWindow w;
+    //w.show();
+
+    QPushButton hello ("Hello world!");
+    hello.resize(100,30);
+    hello.show();
 
     return a.exec();
 }
