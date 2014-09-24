e7f3fcd7c159c4d5d64d14d6c0dc8e7861db6b7b
skewerb <skewerblack@gmail.com>
2014-09-23 19:12:39 -0700

    Added basic slider and graphics window
    They don’t do anything tho! Also need to ignore the build files!

--- Shader_Toggler/main.cpp
+++ Shader_Toggler/main.cpp
@@ -1,16 +1,16 @@
 #include "mainwindow.h"
 #include <QApplication>
-#include <QPushButton>
+//#include <QPushButton>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    //MainWindow w;
-    //w.show();
+    MainWindow w;
+    w.show();
 
-    QPushButton hello ("Hello world!");
+    /*QPushButton hello ("Hello world!");
     hello.resize(100,30);
-    hello.show();
+    hello.show();*/
 
     return a.exec();
 }
